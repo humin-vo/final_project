4990860cfa4b07f69d0936a82dafa871758e27e0
humin-vo <minh.vo2709@gmail.com>
2025-12-21 10:56:19 +0700

    compose with docker

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,9 @@
-# from sqlalchemy import create_engine
+# from sqlalchemy import create_engine, Column, Integer, String
 # from sqlalchemy.orm import sessionmaker, declarative_base
 
-# from sqlalchemy.orm import Session
-# from fastapi import Depends
-
+# # --------------------
+# # DATABASE CONFIG
+# # --------------------
 # DB_USER = "postgres"
 # DB_PASSWORD = "123"
 # DB_HOST = "localhost"
@@ -25,25 +25,35 @@
 
 # Base = declarative_base()
 
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
+# # --------------------
+# # MODEL
+# # --------------------
+# class User(Base):
+#     __tablename__ = "user_info"
 
+#     id = Column(Integer, primary_key=True, index=True)  # SERIAL in DB
+#     name = Column(String, nullable=False)
+#     phone_number = Column(String, nullable=False)
+
+# # Create table if not exists
+# #Base.metadata.create_all(bind=engine)
 
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
 
 # --------------------
 # DATABASE CONFIG
 # --------------------
-DB_USER = "postgres"
-DB_PASSWORD = "123"
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "postgres"
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "123")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME", "postgres")
 
 DATABASE_URL = (
     f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}"
@@ -66,9 +76,6 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "user_info"
 
-    id = Column(Integer, primary_key=True, index=True)  # SERIAL in DB
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     phone_number = Column(String, nullable=False)
-
-# Create table if not exists
-#Base.metadata.create_all(bind=engine)
--- backend/models.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from .database import Base
-
-class User(Base):
-    __tablename__ = "user_info"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    phone_number = Column(String)
